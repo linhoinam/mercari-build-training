10043d01ff77f1bc0adbffedb2712bdd54ab4217
linhoinam <linhoinam.2001@gmail.com>
2025-03-11 22:48:13 +0900

    update sql file

---
+++ python/db/items.sql
@@ -0,0 +1,11 @@
+CREATE TABLE IF NOT EXISTS categories (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL UNIQUE
+);
+CREATE TABLE IF NOT EXISTS items (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL,
+    category_id INTEGER NOT NULL,
+    image_name TEXT,
+    FOREIGN KEY (category_id) REFERENCES categories(id)
+);
